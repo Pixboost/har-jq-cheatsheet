ac0416dc074f760eda386c93990714101542a1a3
Dmitry Pokidov <dooman87@gmail.com>
2018-03-23 07:33:03 +1100

    * Removed total time

--- har-stat.sh
+++ har-stat.sh
@@ -11,14 +11,12 @@ if [ ! -f ${IN} ]; then
 fi
 
 echo Processing HAR file ${IN}
-LIST=$(jq "${BASIC_SELECT} | {url: .request.url, time: .time, size: .response.content.size}" ${IN})
+LIST=$(jq "${BASIC_SELECT} | {url: .request.url, time: .time, size: .response.content.size, t: .timings}" ${IN})
 COUNT=$(jq "[${BASIC_SELECT}] | length" ${IN})
 TOTAL_TIME=$(jq "[ ${BASIC_SELECT} | {time: .time}] | reduce(.[]) as \$i (0; . += \$i.time)" ${IN})
-#REQ_TIME=$(jq "[${BASIC_SELECT} | {time: .time, t: .timings} ] | reduce(.[]) as \$i (0; . += (\$i.t.receive - \$i.t._blocked_queueing + \$i.t.send + \$i.t.wait + \$i.t.blocked))" ${IN})
 BYTES=$(jq "[ ${BASIC_SELECT} | {r: .response} ] | reduce(.[]) as \$i (0; . += \$i.r._transferSize)" ${IN})
 
 echo "${LIST}"
 echo "Number of requests:       ${COUNT}"
 echo "Total time with queueing: ${TOTAL_TIME}"
-#echo "Requests time:            ${REQ_TIME}"
 echo "Bytes:                    ${BYTES}"
