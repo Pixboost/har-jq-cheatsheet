e2b4233c3b8ea20d3e95e3929de287d488502492
Dmitry Pokidov <dooman87@gmail.com>
2018-09-27 07:21:29 +1000

    * Added onContentLoad and onLoad stats

--- har-stat.sh
+++ har-stat.sh
@@ -11,11 +11,15 @@ fi
 
 echo Processing HAR file ${IN}
 
+ON_CONTENT_LOAD=$(jq '.log.pages[0].pageTimings.onContentLoad' ${IN})
+ON_LOAD=$(jq '.log.pages[0].pageTimings.onLoad' ${IN})
 COUNT=$(jq '[ .log.entries[] | select(.request.url | match("png|jpg|PNG|JPG|jpeg|JPEG")) ] | length' ${IN})
 TOTAL_TIME=$(jq '[.log.entries[] | select(.request.url |  match("png|jpg|PNG|JPG|jpeg|JPEG")) | {time: .time}] | reduce(.[]) as $i (0; . += $i.time)' ${IN})
 REQ_TIME=$(jq '[.log.entries[] |select(.request.url | match("png|jpg|PNG|JPG|jpeg|JPEG")) | {time: .time, t: .timings} ] | reduce(.[]) as $i (0; . += ($i.t.receive - $i.t._blocked_queueing + $i.t.send + $i.t.wait + $i.t.blocked))' ${IN})
 BYTES=$(jq '[.log.entries[] |select(.request.url | match("png|jpg|PNG|JPG|jpeg|JPEG")) | {r: .response} ] | reduce(.[]) as $i (0; . += $i.r._transferSize)' ${IN})
 
+echo "On content load:          ${ON_CONTENT_LOAD}"
+echo "On load:                  ${ON_LOAD}"
 echo "Number of requests:       ${COUNT}"
 echo "Total time with queueing: ${TOTAL_TIME}"
 echo "Requests time:            ${REQ_TIME}"
